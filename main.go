4e09d39953dd510472d4bdc31b743a65b2521279
astadipati <asekbos@gmail.com>
2023-01-08 10:36:53 +0700

    set koneksi

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bwastartup/user"
 	"fmt"
 	"log"
 	"os"
-	"os/user"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
@@ -21,11 +21,11 @@ func init() {
 }
 
 func main() {
-	username := os.Getenv("USERE")
-	fmt.Println(username)
-	pass := os.Getenv("PASS")
-	fmt.Println(pass)
-	dsn := "@tcp(127.0.0.1:3306)/bwa_go?charset=utf8mb4&parseTime=True&loc=Local"
+	// username := os.Getenv("USERE")
+	// fmt.Println(username)
+	// pass := os.Getenv("PASS")
+	// fmt.Println(pass)
+	dsn := "root:Kul0nuwun@tcp(127.0.0.1:3306)/bwa_go?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -35,17 +35,17 @@ func main() {
 	fmt.Println("Koneksi Ok")
 
 	var users []user.User
-	// length := len(users)
-	// fmt.Println(length)
+	length := len(users)
+	fmt.Println(length)
 
 	db.Find(&users)
 
-	// length = len(users)
-	// fmt.Println(length)
-	// fmt.Printf("%s hh %s\n", os.Getenv("USER"), os.Getenv("PASSWORD"))
+	length = len(users)
+	fmt.Println(length)
+	fmt.Printf("%s hh %s\n", os.Getenv("USER"), os.Getenv("PASSWORD"))
 
-	// for _, user := range users {
-	// 	fmt.Println(user.Name)
-	// 	fmt.Println(user.Email)
-	// }
+	for _, user := range users {
+		fmt.Println(user.Name)
+		fmt.Println(user.Email)
+	}
 }
